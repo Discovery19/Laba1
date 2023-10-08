e988db21bff3cb17db40d35d9bba9ac3b706ac93
Discovery19 <112725051+Discovery19@users.noreply.github.com>
2023-10-08 22:35:10 +0300

    Лабораторная работа №1
    Мукий Сергей

---
+++ Task2.py
@@ -0,0 +1,5 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+l=len(list_players)
+print(list_players[:l//2])
+print(list_players[l//2:])
+# TODO Разделите участников на две команды
